e669b61c0adcbefae4095adc2e333667c1c1c51a
CoachBossTee <WALKERTESHA81@GMAIL.COM>
2025-12-08 11:58:48 -0500

    Move env vars inside createClient function

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,12 +1,12 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
 export function createClient() {
-  return createSupabaseClient(supabaseUrl!, supabaseKey!);
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase environment variables');
+  }
+
+  return createSupabaseClient(supabaseUrl, supabaseKey);
 }
